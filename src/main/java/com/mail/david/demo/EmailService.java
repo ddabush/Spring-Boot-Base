3fa75caa2d8dd8c0857576826278f5a28be45772
David Dabush <ddabush@gmail.com>
2020-05-03 18:14:13 +0300

    First Commit

--- src/main/java/com/mail/david/demo/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.mail.david.demo;
-
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.util.List;
-
-public interface EmailService {
-    public String sendEmail(String recipientAddress, String subject, String body, String recipientAddressBCC, List<String> filesURLs) throws GeneralSecurityException, IOException;
-}
---
+++ src/main/java/com/main/david/demo/MainController.java
@@ -0,0 +1,36 @@
+package com.main.david.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+@Controller
+@RequestMapping(value = "/main")
+
+public class MainController {
+
+
+    @GetMapping("/health")
+    ResponseEntity health()
+    {
+        ResponseEntity httpResponse = new ResponseEntity("bla bla",HttpStatus.OK);
+        return httpResponse;
+    }
+
+
+    @PostMapping("/send")
+    ResponseEntity send() throws GeneralSecurityException, IOException {
+        ResponseEntity httpResponse = new ResponseEntity(HttpStatus.OK);
+
+        return httpResponse;
+    }
+
+
+
+
+}
