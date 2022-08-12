298b491d9ce7a47efa8f2351bc1ddf4473d6b6a3
david.dabush <david.dabush@cs-robotics.com>
2022-08-12 14:36:09 +0300

    Added tests structure

--- src/main/java/com/base/java/project/MainController.java
+++ src/main/java/com/base/java/project/MainController.java
@@ -1,11 +1,17 @@
 package com.base.java.project;
 
+import logic.Calculate;
+import model.DataToCalculate;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
@@ -23,10 +29,11 @@ public class MainController {
     }
 
 
-    @PostMapping("/send")
-    ResponseEntity send(){
-        ResponseEntity httpResponse = new ResponseEntity(HttpStatus.OK);
-
+    @PostMapping(value = "/calc")
+    public ResponseEntity calculatePow(@RequestBody DataToCalculate number) {
+        Calculate c= new Calculate();
+        Integer result = c.calculatePow(number.getX());
+        ResponseEntity httpResponse = new ResponseEntity(result,HttpStatus.OK);
         return httpResponse;
     }
 
