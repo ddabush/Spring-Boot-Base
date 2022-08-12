8c3fdc50573d52661986ab4eea133a72d80e92da
david.dabush <david.dabush@cs-robotics.com>
2022-08-12 14:46:05 +0300

    Added get

--- src/main/java/com/base/java/project/MainController.java
+++ src/main/java/com/base/java/project/MainController.java
@@ -3,17 +3,10 @@ package com.base.java.project;
 import logic.Calculate;
 import model.DataToCalculate;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-import java.io.IOException;
-import java.security.GeneralSecurityException;
 
 @Controller
 @RequestMapping(value = "/")
@@ -21,10 +14,10 @@ import java.security.GeneralSecurityException;
 public class MainController {
 
 
-    @GetMapping("/health")
-    ResponseEntity health()
+    @GetMapping("/pow3")
+    ResponseEntity pow3(@RequestParam int number, int pow)
     {
-        ResponseEntity httpResponse = new ResponseEntity("bla bla",HttpStatus.OK);
+        ResponseEntity httpResponse = new ResponseEntity(Math.pow(number,pow),HttpStatus.OK);
         return httpResponse;
     }
 
